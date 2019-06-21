1ec3a851294441d8688683afc66de55af8b3d085
lshen365 <lshen1905@gmail.com>
2019-06-20 18:30:00 -0600

    Mouse clickability feature has been added

--- main.cpp
+++ main.cpp
@@ -5,6 +5,12 @@ using namespace std;
 
 
 
+
+
+void decreaseClickSpeed(int currentSpeed) {
+	currentSpeed -= 10;
+}
+
 bool isOn() {
 	if (GetKeyState(VK_MENU) & 0x8000) {
 		if (GetAsyncKeyState(0x4B) & 1) {
@@ -16,16 +22,30 @@ bool isOn() {
 
 }
 
+void click() {
+	INPUT mouse[4] = { 0 };
+	mouse[0].type = INPUT_MOUSE;
+	mouse[1].type = INPUT_MOUSE;
+	mouse[0].mi.dwFlags = MOUSEEVENTF_LEFTDOWN;
+	mouse[1].mi.dwFlags = MOUSEEVENTF_LEFTUP;
+	mouse[2].type = INPUT_MOUSE;
+	mouse[3].type = INPUT_MOUSE;
+	mouse[2].mi.dwFlags = MOUSEEVENTF_LEFTDOWN;
+	mouse[3].mi.dwFlags = MOUSEEVENTF_LEFTUP;
+	SendInput(4, mouse, sizeof(INPUT));
+}
+
 
 //ALT + K == ON
 //ALT + L == OFF
 int main() {
 	bool runClicker = isOn();
+	
 	while (1) {
 		
 		while (runClicker) {
 			cout << "test" << endl;
-
+			click();
 			if (GetKeyState(VK_MENU) & 0x8000) { //Alt + L = OFF
 				if (GetAsyncKeyState(0x4C) & 1) {
 					break;
